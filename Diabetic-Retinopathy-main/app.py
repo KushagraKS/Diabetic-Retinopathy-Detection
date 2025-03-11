f65e3f4f14eb26f0e63fbd99e4c4f7c6c2403e25
Kushagra Srivastava <90953725+KushagraKS@users.noreply.github.com>
2025-03-11 12:53:55 +0530

    Update app.py

--- Diabetic-Retinopathy-main/app.py
+++ Diabetic-Retinopathy-main/app.py
@@ -126,4 +126,4 @@ if __name__ == '__main__':
         app.jinja_env.auto_reload = True
         app.config['TEMPLATES_AUTO_RELOAD'] = True
         app.run(debug=False, host='0.0.0.0', port=port)
-        # app.run(host='0.0.0.0', port=port)
+      
